622aada4c6d906b2828b84d47098225d1ecc9f25
Egil <egil.moller@freecode.no>
2016-03-30 15:17:32 +0200

    Score development bugfixing so it works for purse seiners too

--- utils.py
+++ utils.py
@@ -1,11 +1,12 @@
-import numpy
+import numpy as np
 import os.path
 import math
+import scipy.optimize
 
 # Define some usefull functions
 def clamp(x, low, high):
-    return numpy.where(x <= high,
-        numpy.where(x >= low, x, low),
+    return np.where(x <= high,
+        np.where(x >= low, x, low),
         high)
 
 def center_hist_bins(histres):
@@ -28,12 +29,13 @@ def linear(x, *args):
 
 def mpolynomial(x, *args):
     res = 0
-    for col, colargs in zip(x, numpy.array_split(args, len(x))):
+    for col, colargs in zip(x, np.array_split(args, len(x))):
         res += polynomial(col, *colargs)
     return res
 
 def zigmoid(z):
-    return 1. / (1. + math.e**(-z)) 
+    z = np.where(z < -100, -100, z)
+    return 1. / (1. + np.exp(-z))
 
 def zmpolynomial(x, *args):
     return zigmoid(mpolynomial(x, *args))
@@ -42,10 +44,10 @@ def cached(path):
     def cached(fn):
         def cached(*arg, **kw):
             if os.path.exists(path):
-                return numpy.load(path)['cached']
+                return np.load(path)['cached']
             else:
                 res = fn(*arg, **kw)
-                numpy.savez_compressed(path, cached = res)
+                np.savez_compressed(path, cached = res)
                 return res
         return cached
     return cached
@@ -54,3 +56,44 @@ def fishy(x):
     return x[x["classification"] >= 0.5]
 def nonfishy(x):
     return x[x["classification"] < 0.5]
+
+
+def get_polynomial_cols(x, windows):
+    colnames = []
+    #colnames.append("speed")
+    for window in windows:
+        colnames.append('measure_speedavg_%s' % window)
+        colnames.append('measure_speedstddev_%s_log' % window)
+        colnames.append('measure_coursestddev_%s_log' % window)
+
+    cols = [x[col] for col in colnames]
+
+    return cols
+
+def fit_score(x, windows, grade=4, lambda_val=0.0001):
+    # A pretty straight-forward implementation of regularized
+    # logistical regression without an analytical gradient...
+
+    cols = get_polynomial_cols(x, windows)
+    y = x['classification']
+    m = float(y.shape[0])
+
+    def objective(theta):
+        h = zmpolynomial(cols, *theta)
+
+        a = -y*np.log(np.where(h == 0., 1e-100, h))
+        a = np.where(y == 0., 0., a)
+        b = -(1. - y)*np.log(np.where(h == 1., 1e-100, 1. - h))
+        b = np.where(y == 1., 0., b)
+
+        J = np.sum(a + b) / m
+        Jreg = J + lambda_val * np.sum(theta**2) / (2 * m)
+        return Jreg
+  
+    return scipy.optimize.minimize(objective, [1]*(len(cols)*grade)).x
+
+    # return curve_fit(
+    #     zmpolynomial,
+    #     cols,
+    #     x['classification'],
+    #     [1]*(len(cols)*grade))[0]
